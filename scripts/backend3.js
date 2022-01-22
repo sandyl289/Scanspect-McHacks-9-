653e4e4c8831eb99c7b6dd589532b73f479f9acd
Sandy L <70775435+sandyl289@users.noreply.github.com>
2022-01-22 13:02:42 -0500

    Nothing's working on backend 3

---
+++ scripts/backend3.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path')
+const formidable = require('formidable');
+   
+const app = express();
+   
+app.post('/api/upload', (req, res, next) => {
+    
+    const form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){
+  
+        var oldPath = files.profilePic.path;
+        var newPath = path.join(__dirname, 'uploads')
+                + '/'+files.profilePic.name
+        var rawData = fs.readFileSync(oldPath)
+      
+        fs.writeFile(newPath, rawData, function(err){
+            if(err) console.log(err)
+            return res.send("Successfully uploaded")
+        })
+  })
+});
+
+app.listen(3000, function(err){
+    if(err) console.log(err)
+    console.log('Server listening on Port 3000');
+});
