a92a01cc16e44a075eeba9065d456e2545c11bc5
TylerSyme <tylersyme12@gmail.com>
2022-01-22 14:22:03 -0500

    File upload working again

--- scripts/backend3.js
+++ scripts/backend3.js
@@ -1,31 +1,36 @@
 const express = require('express');
 const fs = require('fs');
-const path = require('path')
-const formidable = require('formidable');   
+const path = require('path');
+const formidable = require('formidable');
+const vision = require('@google-cloud/vision');
+
 const app = express();
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
    
-app.post('/api/upload', (req, res, next) => {
+app.post('/upload', (req, res, next) => {
     
     const form = new formidable.IncomingForm();
     form.parse(req, function(err, fields, files){
-  
-        var oldPath = files.profilePic.path;
-        var newPath = path.join(__dirname, 'uploads')
-                + '/'+files.profilePic.name
-        var rawData = fs.readFileSync(oldPath)
+
+        console.log("files:");
+        console.log(files);
+
+        let newPath = path.join(__dirname, '../uploads') + '/' + files.imgInput.originalFilename;
+        let rawData = fs.readFileSync(files.imgInput.filepath);
       
         fs.writeFile(newPath, rawData, function(err){
-            if(err) console.log(err)
-            return res.send("Successfully uploaded")
+            if (err) {
+              console.log(err);
+              return res.send("Upload failed");
+            } else return res.send("Successfully uploaded")
         })
   })
 });
 
 app.listen(3000, function(err){
     if(err) console.log(err)
-    console.log('Server listening on Port 3000');
+    else console.log('Server listening on Port 3000');
 });
